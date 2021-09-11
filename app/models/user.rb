6a7b0984165f36fbe51c163da11ac539141929b2
chiba97 <ktfkyrya@gmail.com>
2021-09-11 11:08:16 +0000

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :introduction, length: {maximum: 50}
   attachment :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 end
